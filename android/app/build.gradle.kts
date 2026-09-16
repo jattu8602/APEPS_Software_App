793205e7c630b003210cba475befd24a04a4de2c
jattu8602 <chaurasiyajatin68@gmail.com>
2026-09-16 16:02:51 +0530

    chore: update Android NDK version, increase Gradle JVM max heap size, and disable Jetifier

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.calmchase.ssb"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
